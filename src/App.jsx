538cdf7c3b5512b631144ae6a89596d6cf752bb1
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-09-18 06:08:59 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function MyButton() {
   return (
-  <div>
-  
-    <a href="#Home">Home</a>
-    <a href="#products">products</a>
-    <a href="#Details">Details</a>
-    <a href="#prices">prices</a>
-    <a href="#comments">comments</a>
-  
-    <h1><u> welcome </u></h1>
-    <h2>login-page</h2>
-    <input type="email" name="" placeholder='inter your email' id="" />
-    <input type="password" name="" id=""placeholder='enter your pin' />
-    <button>Login</button>
-    
-
-  </div>
-  )
+    <button>
+      I'm a button
+    </button>
+  );
 }
 
-export default App
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
